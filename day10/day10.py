0157e287dbb34c9ded04f4af058d6558a037e2ac
Matteo-Negro <62394493+Matteo-Negro@users.noreply.github.com>
2022-08-25 16:15:02 +0200

    Day10 complete

---
+++ day10/day10.py
@@ -0,0 +1,18 @@
+input = '1113122113'
+
+for _ in range(50):
+    tmp = ''
+    charPrev = ''
+    occurrences = 0
+    for char in input:
+        if charPrev != '' and charPrev != char:
+            tmp += str(occurrences)
+            tmp += charPrev
+            occurrences = 0
+        occurrences += 1
+        charPrev = char
+    tmp += str(occurrences)
+    tmp += charPrev
+    input = tmp
+
+print('DAY10_1 result: ', len(input))
