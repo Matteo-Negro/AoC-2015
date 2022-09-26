018c6d155ea2caf54c5c48fa7363142fba5bef3f
Matteo-Negro <62394493+Matteo-Negro@users.noreply.github.com>
2022-09-26 14:18:12 +0200

    Day14 complete

--- day14/day14.py
+++ day14/day14.py
@@ -1,7 +1,8 @@
 GLOBAL_TIME = 2503
+import numpy as np
 
-with open('input.txt') as file:
-    lines = file.readlines()
+
+def execute_1(lines):
     archive = []
 
     for line in lines:
@@ -21,3 +22,41 @@ with open('input.txt') as file:
 
         archive.sort(reverse=True)
     print('DAY14_1 result: ', archive.pop(0))
+
+
+def execute_2(lines):
+    archive = np.zeros(len(lines))
+
+    for sec in range(1, GLOBAL_TIME + 1):
+        winnerDist = 0
+        winnerLine = []
+        for index, line in enumerate(lines):
+            v = int(line.split(' ')[3])
+            workTime = int(line.split(' ')[6])
+            restTime = int(line.split(' ')[13])
+
+            times = sec / (workTime + restTime)
+            advance = round((times % 1) * (workTime + restTime))
+            distance = 0
+            if advance >= workTime:
+                times += 1
+            else:
+                distance = advance * v
+            distance += ((times // 1) * v * workTime)
+
+            if distance > winnerDist:
+                winnerLine.clear()
+                winnerLine.append(index)
+                winnerDist = distance
+            elif distance == winnerDist:
+                winnerLine.append(index)
+        for winner in winnerLine:
+            archive[winner] += 1
+    print('DAY14_2 result: ', archive.max())
+
+
+with open('input.txt') as file:
+    input = file.readlines()
+
+    execute_1(input)
+    execute_2(input)
