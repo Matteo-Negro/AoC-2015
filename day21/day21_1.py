a6b1e65fcaf1ba16326b71eccf22761173f1ed59
Matteo-Negro <62394493+Matteo-Negro@users.noreply.github.com>
2022-10-11 10:29:10 +0200

    Day21 complete

--- day21/day21_1.py
+++ day21/day21_1.py
@@ -1,39 +1,69 @@
 import mip as mp
+import math
 
+# Game Setup
+# BOSS stats
 BOSS_HIT = 100
 BOSS_DAMAGE = 8
 BOSS_ARMOR = 2
 
+# PLAYER stats
 PLR_HIT = 100
 
-weaponValue = [4, 5, 6, 7, 8]
-weaponCost = [8, 10, 25, 40, 74]
-armorValue = [1, 2, 3, 4, 5]
-armorCost = [13, 31, 53, 75, 102]
-ringValue = [1, 2, 3, 1, 2, 3]
-ringCost = [25, 50, 100, 20, 40, 80]
+# Shop
+weapons = {'Dagger': [4, 8],
+           'Shortsword': [5, 10],
+           'Warhammer': [6, 25],
+           'Longsword': [7, 40],
+           'Greataxe': [8, 74]}
 
+armors = {'Leather': [1, 13],
+          'Chainmail': [2, 31],
+          'Splintmail': [3, 53],
+          'Bandedmail': [4, 75],
+          'Platemail': [5, 102]}
+
+rings = {'Damage +1': [1, 25],
+         'Damage +2': [2, 50],
+         'Damage +3': [3, 100],
+         'Defense +1': [1, 20],
+         'Defense +2': [2, 40],
+         'Defense +3': [3, 80]}
+
+# Model
 model = mp.Model()
 
-w = [model.add_var(name=f'Weapon{i}', var_type=mp.INTEGER, lb=0, ub=1) for i in range(5)]
-a = [model.add_var(name=f'Armor{i}', var_type=mp.INTEGER, lb=0, ub=1) for i in range(5)]
-r = [model.add_var(name=f'Ring{i}', var_type=mp.INTEGER, lb=0, ub=1) for i in range(6)]
+# Variables
+w = [model.add_var(name=f'{key}', var_type=mp.INTEGER, lb=0, ub=1) for key in weapons.keys()]
+a = [model.add_var(name=f'{key}', var_type=mp.INTEGER, lb=0, ub=1) for key in armors.keys()]
+r = [model.add_var(name=f'{key}', var_type=mp.INTEGER, lb=0, ub=1) for key in rings.keys()]
 
-# Feasible contr.
+# Helper operation
+bossAttacks = math.pow(PLR_HIT, -1) * ((BOSS_DAMAGE -
+                                        mp.xsum(a[i] * armors[key][0] for i, key in enumerate(armors.keys())) -
+                                        mp.xsum(r[i] * rings[f'Defense +{i - 2}'][0] for i in range(3, 6))))
+playerAttacks = math.pow(BOSS_HIT, -1) * ((mp.xsum(w[i] * weapons[key][0] for i, key in enumerate(weapons.keys())) +
+                                           mp.xsum(r[i] * rings[f'Damage +{i + 1}'][0] for i in range(3)) - BOSS_ARMOR))
+
+# Feasible constraints
 model.add_constr(mp.xsum(w[i] for i in range(5)) == 1)
 model.add_constr(mp.xsum(a[i] for i in range(5)) <= 1)
 model.add_constr(mp.xsum(r[i] for i in range(6)) <= 2)
-model.add_constr(mp.xsum(w[i] * weaponValue[i] for i in range(5)) +
-                 mp.xsum(r[i] * ringValue[i] for i in range(3)) >= BOSS_ARMOR)
-model.add_constr(((BOSS_DAMAGE - mp.xsum(a[i] * armorValue[i] for i in range(5)) -
-                   mp.xsum(r[i] * ringValue[i] for i in range(3, 6)))) <=
-                 ((mp.xsum(w[i] * weaponValue[i] for i in range(5)) +
-                   mp.xsum(r[i] * ringValue[i] for i in range(3)) - BOSS_ARMOR)))
 
-model.objective = mp.minimize(mp.xsum(w[i] * weaponCost[i] for i in range(5)) +
-                              mp.xsum(a[i] * armorCost[i] for i in range(5)) +
-                              mp.xsum(r[i] * ringCost[i] for i in range(6)))
+# Win constraints
+model.add_constr(mp.xsum(w[i] * weapons[key][0] for i, key in enumerate(weapons.keys())) +
+                 mp.xsum(r[i] * rings[f'Damage +{i + 1}'][0] for i in range(3)) >= BOSS_ARMOR)
+model.add_constr(bossAttacks <= playerAttacks)
+
+# Objective function
+model.objective = mp.minimize(mp.xsum(w[i] * weapons[key][1] for i, key in enumerate(weapons.keys())) +
+                              mp.xsum(a[i] * armors[key][1] for i, key in enumerate(armors.keys())) +
+                              mp.xsum(r[i] * rings[key][1] for i, key in enumerate(rings.keys())))
 
 model.optimize()
 
 print(f'DAY21_1 result: {model.objective.x}')
+
+for item in model.vars:
+    if item.x != 0:
+        print(f'Item: {item}')
